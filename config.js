e1118e2e97832d634c36e995fc9cd91827464dd8
Juliette Rousselet <juliette.roussleet@etu.univ-orleans.fr>
2024-03-28 08:54:56 +0100

    ajout config chemins + ajout data dans la bd

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 export const PERSONNAGES = 'http://localhost:3000/personnages';
 export const BAGUETTES = 'http://localhost:3000/baguettes';
-// export const OBJETS = 'http://localhost:3000/personnages';
+export const ANIMAUX = 'http://localhost:3000/animaux';
+export const OBJETS = 'http://localhost:3000/personnages';
+export const SORTS = 'http://localhost:3000/personnages';
