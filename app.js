c1735e5a52a1ade71bd8b7cd49b301681f91d0ea
Juliette Rousselet <juliette.roussleet@etu.univ-orleans.fr>
2024-04-04 11:51:01 +0200

    start Fix routing issue and add pagination functionality to Personnages view

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const routes = {
     '/home': Home,
     '/personnages': Personnages,
     '/personnages/:id': DetailPerso,
+    '/personnages/pages/:id': Personnages,
 };
 
 const router = async () => {
@@ -17,9 +18,16 @@ const router = async () => {
     let parsedURL = (request.resource ? `/${request.resource}` : '/') + (request.id ? '/:id' : '') + (request.verb ? `/${request.verb}` : '');
     let page = routes[parsedURL] ? new routes[parsedURL] : new Error404;
     console.log(page);
+    console.log(parsedURL);
     if (parsedURL === '/personnages/:id') {
         content.innerHTML = await page.render(request.id);
         await page.after_render(request.id);
+    } else if (page === '/personnages/pages/:id') {
+        content.innerHTML = await page.render(request.id);
+        await page.after_render(request.id);
+    } else if (page === '/personnages/') {
+        content.innerHTML = await page.render();
+        await page.after_render();
     } else {
         content.innerHTML = await page.render();
     }
